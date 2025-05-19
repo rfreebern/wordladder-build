b14540333519ba69bb704e61309ce8e9820f3aaa
Ryan Freebern <ryan@freebern.org>
2025-05-19 15:52:40 -0400

    updated build

--- assets/game.js
+++ assets/game.js
@@ -1,7 +1,37 @@
-globalThis.today = {
-	"ordinal": 1,
-	"start": "BLUFF",
-	"end": "OCEAN",
-	"par": 9,
-	"ace": 7
-}
+globalThis.games = [
+  {
+    "date": "2025-05-19",
+    "start": "ONION",
+    "end": "TEARS",
+    "par": 9,
+    "ace": 7
+  },
+  {
+    "date": "2025-05-20",
+    "start": "ANGRY",
+    "end": "CHILL",
+    "par": 12,
+    "ace": 8
+  },
+  {
+    "date": "2025-05-21",
+    "start": "SONAR",
+    "end": "ECHO",
+    "par": 10,
+    "ace": 7
+  },
+  {
+    "date": "2025-05-22",
+    "start": "SEEDS",
+    "end": "FRUIT",
+    "par": 9,
+    "ace": 6
+  },
+  {
+    "date": "2025-05-23",
+    "start": "TIDAL",
+    "end": "LUNAR",
+    "par": 9,
+    "ace": 6
+  }
+]
