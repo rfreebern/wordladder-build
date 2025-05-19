9d0bf42e72acc5d1e2f822dd25836fbc2cb844ef
Ryan Freebern <ryan@freebern.org>
2025-05-19 16:50:06 -0400

    update game list

--- assets/game.js
+++ assets/game.js
@@ -1,10 +1,10 @@
 globalThis.games = [
   {
     "date": "2025-05-19",
-    "start": "ONION",
-    "end": "TEARS",
-    "par": 9,
-    "ace": 7
+    "start": "EVOLVE",
+    "end": "CHANGE",
+    "par": 12,
+    "ace": 8
   },
   {
     "date": "2025-05-20",
@@ -31,6 +31,706 @@ globalThis.games = [
     "date": "2025-05-23",
     "start": "TIDAL",
     "end": "LUNAR",
+    "par": 9,
+    "ace": 6
+  },
+  {
+    "date": "2025-05-24",
+    "start": "INSULT",
+    "end": "BLOCKED",
+    "par": 13,
+    "ace": 9
+  },
+  {
+    "date": "2025-05-25",
+    "start": "CHILD",
+    "end": "ELDER",
+    "par": 9,
+    "ace": 6
+  },
+  {
+    "date": "2025-05-26",
+    "start": "REALLY",
+    "end": "TRULY",
+    "par": 9,
+    "ace": 6
+  },
+  {
+    "date": "2025-05-27",
+    "start": "PROTON",
+    "end": "NEUTRON",
+    "par": 15,
+    "ace": 11
+  },
+  {
+    "date": "2025-05-28",
+    "start": "POTATO",
+    "end": "FRIES",
+    "par": 10,
+    "ace": 7
+  },
+  {
+    "date": "2025-05-29",
+    "start": "LAYUP",
+    "end": "BASKET",
+    "par": 12,
+    "ace": 8
+  },
+  {
+    "date": "2025-05-30",
+    "start": "CARBS",
+    "end": "PROTEIN",
+    "par": 12,
+    "ace": 8
+  },
+  {
+    "date": "2025-05-31",
+    "start": "VETERAN",
+    "end": "HONOR",
+    "par": 13,
+    "ace": 9
+  },
+  {
+    "date": "2025-06-01",
+    "start": "SEEDLING",
+    "end": "TREE",
+    "par": 12,
+    "ace": 8
+  },
+  {
+    "date": "2025-06-02",
+    "start": "TWISTY",
+    "end": "PASSAGE",
+    "par": 21,
+    "ace": 16
+  },
+  {
+    "date": "2025-06-03",
+    "start": "LEAKS",
+    "end": "INFLATES",
+    "par": 12,
+    "ace": 8
+  },
+  {
+    "date": "2025-06-04",
+    "start": "STRAINS",
+    "end": "SOOTHES",
+    "par": 10,
+    "ace": 7
+  },
+  {
+    "date": "2025-06-05",
+    "start": "FORWARD",
+    "end": "REVERSE",
+    "par": 15,
+    "ace": 11
+  },
+  {
+    "date": "2025-06-06",
+    "start": "PROOF",
+    "end": "PUDDING",
+    "par": 13,
+    "ace": 9
+  },
+  {
+    "date": "2025-06-07",
+    "start": "AUDIBLE",
+    "end": "SILENT",
+    "par": 9,
+    "ace": 6
+  },
+  {
+    "date": "2025-06-08",
+    "start": "MENTOR",
+    "end": "GROWTH",
+    "par": 10,
+    "ace": 7
+  },
+  {
+    "date": "2025-06-09",
+    "start": "CHILLY",
+    "end": "THEATRE",
+    "par": 13,
+    "ace": 9
+  },
+  {
+    "date": "2025-06-10",
+    "start": "FEARING",
+    "end": "FAULT",
+    "par": 12,
+    "ace": 8
+  },
+  {
+    "date": "2025-06-11",
+    "start": "SLOPPY",
+    "end": "STEAK",
+    "par": 9,
+    "ace": 6
+  },
+  {
+    "date": "2025-06-12",
+    "start": "CURSE",
+    "end": "BIRTH",
+    "par": 9,
+    "ace": 6
+  },
+  {
+    "date": "2025-06-13",
+    "start": "BABEL",
+    "end": "VOTING",
+    "par": 12,
+    "ace": 8
+  },
+  {
+    "date": "2025-06-14",
+    "start": "QUERIES",
+    "end": "BOOTHS",
+    "par": 14,
+    "ace": 10
+  },
+  {
+    "date": "2025-06-15",
+    "start": "GENERIC",
+    "end": "CORPSE",
+    "par": 14,
+    "ace": 10
+  },
+  {
+    "date": "2025-06-16",
+    "start": "YOUNGER",
+    "end": "ADOPT",
+    "par": 13,
+    "ace": 9
+  },
+  {
+    "date": "2025-06-17",
+    "start": "SUMMERS",
+    "end": "LENDER",
+    "par": 12,
+    "ace": 8
+  },
+  {
+    "date": "2025-06-18",
+    "start": "SHIFTING",
+    "end": "SLUDGE",
+    "par": 14,
+    "ace": 10
+  },
+  {
+    "date": "2025-06-19",
+    "start": "TURNED",
+    "end": "PIERCE",
+    "par": 8,
+    "ace": 5
+  },
+  {
+    "date": "2025-06-20",
+    "start": "SELECTED",
+    "end": "ESTATE",
+    "par": 12,
+    "ace": 8
+  },
+  {
+    "date": "2025-06-21",
+    "start": "AWARDED",
+    "end": "HOMER",
+    "par": 9,
+    "ace": 6
+  },
+  {
+    "date": "2025-06-22",
+    "start": "AMENDED",
+    "end": "UNTITLED",
+    "par": 10,
+    "ace": 7
+  },
+  {
+    "date": "2025-06-23",
+    "start": "SHRINE",
+    "end": "NOODLES",
+    "par": 12,
+    "ace": 8
+  },
+  {
+    "date": "2025-06-24",
+    "start": "WHOLE",
+    "end": "SMOOTHER",
+    "par": 10,
+    "ace": 7
+  },
+  {
+    "date": "2025-06-25",
+    "start": "POUCHES",
+    "end": "GLANCE",
+    "par": 10,
+    "ace": 7
+  },
+  {
+    "date": "2025-06-26",
+    "start": "OUTLINE",
+    "end": "WAKING",
+    "par": 13,
+    "ace": 9
+  },
+  {
+    "date": "2025-06-27",
+    "start": "SIGHT",
+    "end": "FRICTION",
+    "par": 14,
+    "ace": 10
+  },
+  {
+    "date": "2025-06-28",
+    "start": "BILLIONS",
+    "end": "ATTACKER",
+    "par": 20,
+    "ace": 15
+  },
+  {
+    "date": "2025-06-29",
+    "start": "TRIAD",
+    "end": "FOLEY",
+    "par": 9,
+    "ace": 6
+  },
+  {
+    "date": "2025-06-30",
+    "start": "BOURNE",
+    "end": "RETURNED",
+    "par": 12,
+    "ace": 8
+  },
+  {
+    "date": "2025-07-01",
+    "start": "NIMROD",
+    "end": "PRESIDENT",
+    "par": 17,
+    "ace": 13
+  },
+  {
+    "date": "2025-07-02",
+    "start": "BILLIE",
+    "end": "POSING",
+    "par": 10,
+    "ace": 7
+  },
+  {
+    "date": "2025-07-03",
+    "start": "RAPID",
+    "end": "SKIPPED",
+    "par": 8,
+    "ace": 5
+  },
+  {
+    "date": "2025-07-04",
+    "start": "STARS",
+    "end": "STRIPES",
+    "par": 7,
+    "ace": 4
+  },
+  {
+    "date": "2025-07-05",
+    "start": "PASSED",
+    "end": "STARK",
+    "par": 8,
+    "ace": 5
+  },
+  {
+    "date": "2025-07-06",
+    "start": "PROVERBS",
+    "end": "PUMPKIN",
+    "par": 16,
+    "ace": 12
+  },
+  {
+    "date": "2025-07-07",
+    "start": "RECITAL",
+    "end": "FLOCK",
+    "par": 10,
+    "ace": 7
+  },
+  {
+    "date": "2025-07-08",
+    "start": "FLOATS",
+    "end": "CHOSE",
+    "par": 9,
+    "ace": 6
+  },
+  {
+    "date": "2025-07-09",
+    "start": "NOVELS",
+    "end": "TRANCE",
+    "par": 10,
+    "ace": 7
+  },
+  {
+    "date": "2025-07-10",
+    "start": "FISHES",
+    "end": "DWELLINGS",
+    "par": 13,
+    "ace": 9
+  },
+  {
+    "date": "2025-07-11",
+    "start": "DEPARTING",
+    "end": "BESIDE",
+    "par": 15,
+    "ace": 11
+  },
+  {
+    "date": "2025-07-12",
+    "start": "FARMING",
+    "end": "WEAVE",
+    "par": 10,
+    "ace": 7
+  },
+  {
+    "date": "2025-07-13",
+    "start": "ENDORSED",
+    "end": "BARKER",
+    "par": 13,
+    "ace": 9
+  },
+  {
+    "date": "2025-07-14",
+    "start": "HOCKEY",
+    "end": "DROPS",
+    "par": 9,
+    "ace": 6
+  },
+  {
+    "date": "2025-07-15",
+    "start": "LOOTERS",
+    "end": "VILLA",
+    "par": 10,
+    "ace": 7
+  },
+  {
+    "date": "2025-07-16",
+    "start": "WARDEN",
+    "end": "REALLY",
+    "par": 9,
+    "ace": 6
+  },
+  {
+    "date": "2025-07-17",
+    "start": "MATTE",
+    "end": "MEDIUM",
+    "par": 10,
+    "ace": 7
+  },
+  {
+    "date": "2025-07-18",
+    "start": "SENSORY",
+    "end": "SAVER",
+    "par": 9,
+    "ace": 6
+  },
+  {
+    "date": "2025-07-19",
+    "start": "COUSINS",
+    "end": "TUFTS",
+    "par": 13,
+    "ace": 9
+  },
+  {
+    "date": "2025-07-20",
+    "start": "DEEPEST",
+    "end": "LANCE",
+    "par": 12,
+    "ace": 8
+  },
+  {
+    "date": "2025-07-21",
+    "start": "COPYING",
+    "end": "LOVING",
+    "par": 6,
+    "ace": 3
+  },
+  {
+    "date": "2025-07-22",
+    "start": "ENTAIL",
+    "end": "CREATURE",
+    "par": 15,
+    "ace": 11
+  },
+  {
+    "date": "2025-07-23",
+    "start": "METALS",
+    "end": "ROOFING",
+    "par": 13,
+    "ace": 9
+  },
+  {
+    "date": "2025-07-24",
+    "start": "ANGELS",
+    "end": "ROBUST",
+    "par": 12,
+    "ace": 8
+  },
+  {
+    "date": "2025-07-25",
+    "start": "CURED",
+    "end": "INCOME",
+    "par": 12,
+    "ace": 8
+  },
+  {
+    "date": "2025-07-26",
+    "start": "GODDESS",
+    "end": "REALM",
+    "par": 12,
+    "ace": 8
+  },
+  {
+    "date": "2025-07-27",
+    "start": "CONTEST",
+    "end": "WINNER",
+    "par": 8,
+    "ace": 5
+  },
+  {
+    "date": "2025-07-28",
+    "start": "PARADOX",
+    "end": "ENIGMA",
+    "par": 15,
+    "ace": 11
+  },
+  {
+    "date": "2025-07-29",
+    "start": "GENERAL",
+    "end": "REFILL",
+    "par": 10,
+    "ace": 7
+  },
+  {
+    "date": "2025-07-30",
+    "start": "GROWERS",
+    "end": "QUARTZ",
+    "par": 13,
+    "ace": 9
+  },
+  {
+    "date": "2025-07-31",
+    "start": "INTERNET",
+    "end": "ROUTER",
+    "par": 12,
+    "ace": 8
+  },
+  {
+    "date": "2025-08-01",
+    "start": "RELIANCE",
+    "end": "CASTE",
+    "par": 13,
+    "ace": 9
+  },
+  {
+    "date": "2025-08-02",
+    "start": "COMPLY",
+    "end": "WARNED",
+    "par": 10,
+    "ace": 7
+  },
+  {
+    "date": "2025-08-03",
+    "start": "FLYER",
+    "end": "PASSED",
+    "par": 9,
+    "ace": 6
+  },
+  {
+    "date": "2025-08-04",
+    "start": "EXPRESS",
+    "end": "SHELTER",
+    "par": 14,
+    "ace": 10
+  },
+  {
+    "date": "2025-08-05",
+    "start": "THEREOF",
+    "end": "HEALTH",
+    "par": 13,
+    "ace": 9
+  },
+  {
+    "date": "2025-08-06",
+    "start": "NUMBER",
+    "end": "BEDROOM",
+    "par": 13,
+    "ace": 9
+  },
+  {
+    "date": "2025-08-07",
+    "start": "CRITICS",
+    "end": "DONKEY",
+    "par": 15,
+    "ace": 11
+  },
+  {
+    "date": "2025-08-08",
+    "start": "HOPPER",
+    "end": "TUNNING",
+    "par": 13,
+    "ace": 9
+  },
+  {
+    "date": "2025-08-09",
+    "start": "RETIRED",
+    "end": "TURBO",
+    "par": 13,
+    "ace": 9
+  },
+  {
+    "date": "2025-08-10",
+    "start": "ACTION",
+    "end": "SPEAKS",
+    "par": 12,
+    "ace": 8
+  },
+  {
+    "date": "2025-08-11",
+    "start": "EMACS",
+    "end": "EAGLE",
+    "par": 8,
+    "ace": 5
+  },
+  {
+    "date": "2025-08-12",
+    "start": "LETTER",
+    "end": "LETTERING",
+    "par": 12,
+    "ace": 8
+  },
+  {
+    "date": "2025-08-13",
+    "start": "WRITE",
+    "end": "REVISE",
+    "par": 8,
+    "ace": 5
+  },
+  {
+    "date": "2025-08-14",
+    "start": "COLONY",
+    "end": "RESIDES",
+    "par": 12,
+    "ace": 8
+  },
+  {
+    "date": "2025-08-15",
+    "start": "REGULATED",
+    "end": "CIRCUS",
+    "par": 16,
+    "ace": 12
+  },
+  {
+    "date": "2025-08-16",
+    "start": "VIVID",
+    "end": "DARLING",
+    "par": 12,
+    "ace": 8
+  },
+  {
+    "date": "2025-08-17",
+    "start": "PLACING",
+    "end": "SHAPE",
+    "par": 12,
+    "ace": 8
+  },
+  {
+    "date": "2025-08-18",
+    "start": "SPELLS",
+    "end": "TEENAGERS",
+    "par": 16,
+    "ace": 12
+  },
+  {
+    "date": "2025-08-19",
+    "start": "SCAMPS",
+    "end": "STRIPED",
+    "par": 9,
+    "ace": 6
+  },
+  {
+    "date": "2025-08-20",
+    "start": "SHIRT",
+    "end": "SHOPPERS",
+    "par": 9,
+    "ace": 6
+  },
+  {
+    "date": "2025-08-21",
+    "start": "BABES",
+    "end": "REIGN",
+    "par": 9,
+    "ace": 6
+  },
+  {
+    "date": "2025-08-22",
+    "start": "BATTER",
+    "end": "COOKIES",
+    "par": 10,
+    "ace": 7
+  },
+  {
+    "date": "2025-08-23",
+    "start": "CHILLING",
+    "end": "ETHER",
+    "par": 14,
+    "ace": 10
+  },
+  {
+    "date": "2025-08-24",
+    "start": "LISTENERS",
+    "end": "CRACKS",
+    "par": 13,
+    "ace": 9
+  },
+  {
+    "date": "2025-08-25",
+    "start": "HYDRO",
+    "end": "VALVE",
+    "par": 10,
+    "ace": 7
+  },
+  {
+    "date": "2025-08-26",
+    "start": "PROFESSOR",
+    "end": "EMERITUS",
+    "par": 18,
+    "ace": 14
+  },
+  {
+    "date": "2025-08-27",
+    "start": "DRAINAGE",
+    "end": "SALLE",
+    "par": 13,
+    "ace": 9
+  },
+  {
+    "date": "2025-08-28",
+    "start": "BUSTER",
+    "end": "SHRUBS",
+    "par": 9,
+    "ace": 6
+  },
+  {
+    "date": "2025-08-29",
+    "start": "HANDSETS",
+    "end": "FUNNIEST",
+    "par": 13,
+    "ace": 9
+  },
+  {
+    "date": "2025-08-30",
+    "start": "DEALERS",
+    "end": "TURTLE",
+    "par": 13,
+    "ace": 9
+  },
+  {
+    "date": "2025-08-31",
+    "start": "LEADER",
+    "end": "SERVANT",
     "par": 9,
     "ace": 6
   }
